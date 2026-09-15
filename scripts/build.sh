608e5d7fe61daba5757cc3c801d6d798f8107439
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-09-14 22:27:00 -0700

    full node basic build

--- scripts/build.sh
+++ scripts/build.sh
@@ -118,4 +118,9 @@ run bin/fam.uncompressed bin/fam0 src/fam.fam0
 run bin/fampack bin/fam.uncompressed lib/stdlib.fam lib/asm.fam src/fampack.fam
 pack bin/fam.uncompressed bin/fam
 run bin/gen_hash bin/fam lib/stdlib.fam lib/asm.fam src/gen_hash.fam
-
+run bin/tabernacle.uncompressed bin/fam0 src/tabernacle.fam0
+run bin/full_node.bin \
+	bin/fam \
+	lib/stdlib.fam \
+	lib/build.fam \
+	`cat scripts/files.txt`
